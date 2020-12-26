aedf3570000e7604f79f17407513c496670e2866
Nattawut <thisnattawut@gmail.com>
2020-12-26 23:05:39 +0700

    edit main

--- src/main/java/th/ac/ku/atm/Main.java
+++ src/main/java/th/ac/ku/atm/Main.java
@@ -5,23 +5,10 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("bean.xml");
-        ATM atm = context.getBean(ATM.class);
+        ApplicationContext context=
+                new ClassPathXmlApplicationContext("bean.xml");
 
-        String name = atm.validateCustomer(3,7899);
-        Bank bank = context.getBean(Bank.class);
-        System.out.println("Welcome to "+bank.getName()+"!");
-        int dep = 9985;
-        int wit = 2165;
-
-        System.out.println(name+" current balance is "+atm.getBalance()+" THB");
-
-        atm.deposit(dep);
-        System.out.println(name+" has deposit " + dep +" THB");
-        System.out.println(name+" has "+atm.getBalance());
-
-        atm.withdraw(wit);
-        System.out.println(name+" has withdraw " + wit +" THB");
-        System.out.println(name+" has "+atm.getBalance());
+        AtmUI atmUI = context.getBean(AtmUI.class);
+        atmUI.run();
     }
 }
